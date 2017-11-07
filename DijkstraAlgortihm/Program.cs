5592d5490171404aed1446018f9e7812cf09b5c7
Silver Soul <nhankimthanh1997@gmail.com>
2017-11-08 01:04:40 +0700

    Add files via upload

---
+++ DijkstraAlgortihm/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DijkstraAlgortihm
+{
+    class Program
+    {
+        public static LinkedList<Tuple<int, int>>[] ds;
+        public static int n;
+        public static int xp;
+        public static int kt;
+        public static bool[] visited;
+        public static Queue<int> q;
+        public static int[] dist;
+        public static int[] pre;
+        public static bool[] label;
+
+        public static void Input()
+        {
+            StreamReader sr = new StreamReader("graph.inp");
+            string[] chuoi = sr.ReadLine().Trim().Split(' ');
+            n = int.Parse(chuoi[0]);
+            xp = int.Parse(chuoi[1]);
+            kt = int.Parse(chuoi[2]);
+            dist = new int[n];
+            pre = new int[n];
+            label = new bool[n];
+            ds = new LinkedList<Tuple<int, int>>[n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = sr.ReadLine().Trim().Split(' ');
+                ds[i] = new LinkedList<Tuple<int, int>>();
+                /* cach 1 */
+                for (int j = 0; j < s.Count(); j++)
+                {
+                    ds[i].AddLast(new Tuple<int, int>(int.Parse(s[j]), int.Parse(s[j + 1])));
+                    j++;
+                }
+                #region cach 2
+                //for (int j = 0; j < s.Count() / 2; j++)
+                //{
+                //    ds[i].AddLast(new Tuple<int, int>(int.Parse(s[2*j]), int.Parse(s[2*j + 1])));
+                //}
+                #endregion
+            }
+            sr.Close();
+        }
+        public static void Output()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                foreach (var item in ds[i])
+                    Console.Write(item.Item1 + " " + item.Item2 + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public static void DFS(int x)
+        {
+            if (visited[x]) return;
+            visited[x] = true;
+            q.Enqueue(x);
+            foreach (var item in ds[x])
+            {
+                DFS(item.Item1 - 1);
+            }
+        }
+        public static void PrintDFS()
+        {
+            Console.Write(">> DFS list: ");
+            while (q.Count() != 0)
+                Console.Write(q.Dequeue() + 1 + " ");
+            Console.WriteLine();
+        }
+
+        public static void SetInfiniteAllDistElement()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                pre[i] = -1;
+                dist[i] = int.MaxValue;
+            }
+        }
+        public static void Solve()
+        {
+            DFS(xp - 1);
+            PrintDFS();
+            if (visited[kt - 1] == false)
+                Console.WriteLine("No road from {0} to {1}!", xp, kt);
+            else
+            {
+                Dijkstra();
+                ShortestRoad(kt - 1);
+            }
+            Console.WriteLine();
+        }
+        public static int Min()
+        {
+            int min = -1;
+            for(int i = 0; i < n ; i++)
+            {
+                if(min == -1)
+                {
+                    if (label[i] == true)
+                        continue;
+                    else
+                        min = i;
+                }
+                if (dist[i] < dist[min] && label[i] == false)
+                    min = i;
+            }
+            return min;
+        }
+        public static void Dijkstra()
+        {
+            SetInfiniteAllDistElement();
+            dist[xp - 1] = 0;
+            int start = -1;
+            while (label[kt - 1] == false)
+            {
+                start = Min() + 1;
+                foreach (var item in ds[start - 1])
+                {
+                    if (label[item.Item1 - 1] == false)
+                    {
+                        if (dist[item.Item1 - 1] > dist[start - 1] + item.Item2)
+                        {
+                            dist[item.Item1 - 1] = dist[start - 1] + item.Item2;
+                            pre[item.Item1 - 1] = start - 1;
+                        }
+                    }
+
+                }
+                label[Min()] = true;
+            }
+        }
+        public static void ShortestRoad(int finish)
+        {
+            Console.Write("Shortest road are [" + dist[finish] + "] with this road : ");
+            Stack<int> road = new Stack<int>();
+            while (true)
+            {
+                road.Push(finish + 1);
+                if (pre[finish] == -1)
+                    break;
+                finish = pre[finish];
+            }
+            while(road.Count != 0)
+                Console.Write(road.Pop() + " ");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Input();
+            Output();
+            q = new Queue<int>();
+            visited = new bool[n];
+            Solve();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -41,7 +41,7 @@ hàm thiết lập cho các `dist[i]` giá trị lớn nhất là vô cực và 
 hàm kiểm tra đồ thị bằng `DFS()`. Nếu điểm kết thúc không liên thông với điểm bắt đầu thì không thể chạy được, ngược lại sẽ chạy hàm `dijkstra()` và in ra `ShortestRoad()`
 > Min()
 
-đây là hàm tìm giá trị nhỏ nhất trong `int[] dist`
+đây là hàm tìm giá trị nhỏ nhất trong `int[] dist`. Hàm này trả về *vị trí* của phần tử
 ```csharp
 if(min == -1)
 {
@@ -53,6 +53,33 @@ if(min == -1)
 if (dist[i] < dist[min] && label[i] == false)
     min = i;
 ```
-nếu phần tử nhỏ nhất đã được xác định thì chuyễn sang phần tử nhỏ kế nó. Ví dụ
-`0 1 2 3 4 5 6`
-`T T F F F F F`
+nếu phần tử nhỏ nhất đã được xác định thì chuyễn sang phần tử nhỏ kế nó. Ví dụ chuỗi sau:
+```
+0 1 2 3 4 5 6
+T T F F F F F
+```
+phần tử `0` nhỏ nhất nhưng nó là `T` nên `continue` sang `1` và `1` cũng `T` nên chuyển cho phần tử nhỏ tiếp là `2` và có `F` nên nhận `min = 2`
+> Dijkstra()
+
+1. gọi hàm `SetInfiniteAllDistElement()` để thiết lập.
+2. phần tử xuất phát = 0
+3. khai báo biến `start` - biến tạm cho vị trí xuất phát
+4. Bắt đầu từ phần tử nhỏ nhất với hàm `Min()`
+5. Tìm các phần tử kề với đỉnh `start`
+6. Nếu phần tử kề này chưa đánh dấu thì :
+7. Kiểm tra và cập nhật lại `dist[i]` và tạo `pre[i]` có giá trị là phần tử root
+```Csharp
+if (dist[item.Item1 - 1] > dist[start - 1] + item.Item2)
+{
+    dist[item.Item1 - 1] = dist[start - 1] + item.Item2;
+    pre[item.Item1 - 1] = start - 1;
+}
+```
+8. Gán thẻ xác định cho phần tử nhỏ nhất để lần sau không đụng tới (loại bỏ khỏi tập đồ thị).
+9. Lặp lại bước 4 cho tới khi phần tử kết thúc không còn nằm trong tập đồ thị.
+
+> ShortestRoad()
+
+hàm hiển thị đường đi của dijkstra bằng `Stack`
+
+===========================================================
